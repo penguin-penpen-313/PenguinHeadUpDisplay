52bfc210fb4745bed904551614f2d44aff7dc7a5
penguin-penpen-313 <penguinpnpn@gmail.com>
2026-03-24 18:23:34 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE  = 'hud-v1.0.0';
+const CACHE  = 'hud-v1.0.1';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
